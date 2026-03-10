05176151125178755fd0ca3be20a37fe8dda4038
SergioArjonaRamírez <sergioarjonadam@gmail.com>
2026-03-10 08:36:52 +0100

    Initialize Spring Boot project with Maven wrapper and MongoDB configuration

---
+++ src/main/java/org/example/examen_5_6_ad_sergioarjona/Examen56AdSergioArjonaApplication.java
@@ -0,0 +1,13 @@
+package org.example.examen_5_6_ad_sergioarjona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen56AdSergioArjonaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Examen56AdSergioArjonaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/examen_5_6_ad_sergioarjona/Examen56AdSergioArjonaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.examen_5_6_ad_sergioarjona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen56AdSergioArjonaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
